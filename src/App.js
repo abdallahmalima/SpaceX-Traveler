0567c6704ad267bfb10690c57615096d0fb35bcc
Mukumbuta <mukumbutasimasiku@gmail.com>
2022-12-21 13:35:08 +0200

    enabked routing functionality

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Rockets from './pages/Rockets';
+import Missions from './pages/Missions';
+import Profile from './pages/Profile';
 
 const App = () => (
-  <div className="">
-    <h1>Hello SpaceX</h1>
-  </div>
+  <Router>
+    <Navbar />
+    <Routes>
+      <Route exact path="/" element={<Rockets />} />
+      <Route exact path="/rockets" element={<Rockets />} />
+      <Route exact path="/missions" element={<Missions />} />
+      <Route exact path="/profile" element={<Profile />} />
+    </Routes>
+  </Router>
 );
 
 export default App;
