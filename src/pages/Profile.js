009f51229debf089c83414e2c98edb27d67c4144
Mukumbuta <mukumbutasimasiku@gmail.com>
2022-12-22 12:20:36 +0200

    fetch and display missions

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import MyProfile from '../components/Profile';
+import ProfileSection from '../components/MyProfile';
 
 const Profile = () => (
   <div>
-    <MyProfile />
+    <ProfileSection />
   </div>
 );
 
