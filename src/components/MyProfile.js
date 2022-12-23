70fed9e061637560bdb6fe02006c502bae1d42d0
abdallah <abdallahantony55.aa@gmail.com>
2022-12-23 15:44:54 +0300

    Added rocket mission

--- src/components/MyProfile.js
+++ src/components/MyProfile.js
@@ -1,16 +1,23 @@
 import React from 'react';
 import Button from 'react-bootstrap/Button';
+import { useSelector } from 'react-redux';
 
-const MyProfile = () => (
-  <div className="profile">
-    <div>
-      <h3>My Mission</h3>
+const MyProfile = () => {
+  const rockets = useSelector((state) => state.rockets.rockets.filter());
+  return (
+    <div className="profile">
+      <div>
+        <h3>My Mission</h3>
+        {rockets.map((rocket) => (
+          <p key={rocket.id}>{rocket.rocketName}</p>
+        ))}
+      </div>
+      <div>
+        <h3>My Rockets</h3>
+        <Button type="button" className="btn btn-primary">Login here</Button>
+      </div>
     </div>
-    <div>
-      <h3>My Rockets</h3>
-      <Button type="button" className="btn btn-primary">Login here</Button>
-    </div>
-  </div>
-);
+  );
+};
 
 export default MyProfile;
