6d9cdf96b67946714545eca5ba20135858943ee9
abdallah <abdallahantony55.aa@gmail.com>
2022-12-24 18:32:58 +0300

    Add test to reducers

--- src/components/rockets/rocketList.js
+++ src/components/rockets/rocketList.js
@@ -6,6 +6,8 @@ import rocketStyles from './rocket.module.css';
 
 const RocketList = () => {
   const rockets = useSelector((state) => state.rockets.rockets);
+  const errorMessage = useSelector((state) => state.rockets.error);
+  const isLoading = useSelector((state) => state.rockets.loading);
   const dispatch = useDispatch();
   useEffect(() => {
     if (rockets.length === 0) dispatch(fetchRockets());
@@ -13,6 +15,8 @@ const RocketList = () => {
 
   return (
     <main className={rocketStyles.container}>
+      {isLoading && <h3 className="textError">Loading...</h3>}
+      {errorMessage && <h3 className="textError">{errorMessage}</h3>}
       {rockets.map((rocket) => (
         <Rocket key={rocket.id} rocket={rocket} />
       ))}
--- src/redux/rockets/rocketsSlice.js
+++ src/redux/rockets/rocketsSlice.js
@@ -1,32 +1,40 @@
 import { createSlice } from '@reduxjs/toolkit';
 import fetchRockets from './rocketsThunk';
 
-const initialState = {
+export const initialState = {
   rockets: [],
   loading: true,
-  error: '',
+  error: null,
+};
+
+const updateReservationStatus = (state, action, reserved) => {
+  state.rockets = state.rockets.map((rocket) => {
+    if (rocket.id !== action.payload) return rocket;
+    return { ...rocket, reserved };
+  });
 };
 
 const rocketsSlice = createSlice({
   name: 'rockets',
   initialState,
   reducers: {
-    reserveRocket(state, action) {
-      state.rockets = state.rockets.map((rocket) => {
-        if (rocket.id !== action.payload) return rocket;
-        return { ...rocket, reserved: true };
-      });
-    },
-    cancelReservation(state, action) {
-      state.rockets = state.rockets.map((rocket) => {
-        if (rocket.id !== action.payload) return rocket;
-        return { ...rocket, reserved: false };
-      });
-    },
+    reserveRocket: (state, action) => updateReservationStatus(state, action, true),
+    cancelReservation: (state, action) => updateReservationStatus(state, action, false),
   },
   extraReducers: (builder) => {
+    builder.addCase(fetchRockets.pending, (state) => {
+      state.error = null;
+      state.loading = true;
+    });
     builder.addCase(fetchRockets.fulfilled, (state, action) => {
+      state.error = null;
       state.rockets = action.payload;
+      state.loading = false;
+    });
+
+    builder.addCase(fetchRockets.rejected, (state, action) => {
+      state.error = action.error.message;
+      state.loading = false;
     });
   },
 });
