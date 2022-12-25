062e201a51233b9c3dfdbc41ca7f39b83e0063ab
abdallah <abdallahantony55.aa@gmail.com>
2022-12-25 08:19:48 +0300

    Add toastr Message on createand cancle reservation

--- src/components/rockets/rocket.js
+++ src/components/rockets/rocket.js
@@ -1,5 +1,6 @@
 import PropTypes from 'prop-types';
 import { useDispatch } from 'react-redux';
+import { toast } from 'react-toastify';
 import { cancelReservation, reserveRocket } from '../../redux/rockets/rocketsSlice';
 import rocketStyles from './rocket.module.css';
 
@@ -9,17 +10,25 @@ const Rocket = ({
   },
 }) => {
   const dispatch = useDispatch();
+  const showToastr = (msg) => {
+    toast(msg, {
+      type: 'success',
+    });
+  };
 
   const handleReservation = (id) => {
     if (!reserved) {
       dispatch(reserveRocket(id));
+      showToastr('The rocket was reserved successfully.');
     } else {
       dispatch(cancelReservation(id));
+      showToastr('The reservation was cancelled successfully.');
     }
   };
 
   return (
     <main className={rocketStyles.rocketCard}>
+
       <img src={flickrImage} className={rocketStyles.img} alt="Rocket" />
       <div className={rocketStyles.details}>
         <h4>{rocketName}</h4>
